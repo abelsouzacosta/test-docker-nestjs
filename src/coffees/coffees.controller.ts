76da46f211e6480d455c5ef909c29c6ebc168fae
junior <abelsouzacosta@gmail.com>
2022-08-28 18:19:09 -0300

    refactor(coffees): controller
    handling response status code

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get()
+  getAll(): string {
+    return `this method returns all coffees in the database`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): string {
+    return `this method return the ${id} instance`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() data: any) {
+    return data;
+  }
+}
