ab5a69c2c02a18045802a61ffbbb6e3be3bc26c5
junior <abelsouzacosta@gmail.com>
2022-08-28 18:19:57 -0300

    refactor(coffees): controller
    improve status code responses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,11 +11,13 @@ import {
 @Controller('coffees')
 export class CoffeesController {
   @Get()
+  @HttpCode(HttpStatus.OK)
   getAll(): string {
     return `this method returns all coffees in the database`;
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   getOne(@Param('id') id: string): string {
     return `this method return the ${id} instance`;
   }
